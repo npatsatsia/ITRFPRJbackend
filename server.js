81795dfa73981a0ba4968e28425dd32bb4631664
npatsatsia <npatsatsia1@gmail.com>
2024-01-25 23:57:39 +0300

    commito

--- server.js
+++ server.js
@@ -95,12 +95,3 @@ const startServer = async () => {
 
 startServer();
 
-// Your server response middleware
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
-
