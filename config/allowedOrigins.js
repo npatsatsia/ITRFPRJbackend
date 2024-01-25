10fa2dff2ee1040648837fa75a5411783ef1ec34
npatsatsia <npatsatsia1@gmail.com>
2024-01-26 00:54:17 +0300

    commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000', 'https://itra-prj.vercel.app']
+const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000', 'https://itra-prj.vercel.app', 'https://itrabackend-0a797af92f8e.herokuapp.com']
 
 module.exports = allowedOrigins
