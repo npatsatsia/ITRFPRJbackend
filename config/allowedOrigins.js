f2a55471e0e66b9e619c4e7ee90616449d258506
npatsatsia <npatsatsia1@gmail.com>
2024-01-26 13:35:54 +0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://127.0.0.1:5500', 'https://itra-app.vercel.app', 'https://itraapp-0fceed09a1ce.herokuapp.com']
+const allowedOrigins = ['http://127.0.0.1:5500', 'https://itra-app.vercel.app', 'https://itrabackend-0a797af92f8e.herokuapp.com']
 
 module.exports = allowedOrigins
